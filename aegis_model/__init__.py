47823dcacfd154338b6cfb392229f3d0373d733f
rahuldhal800-a11y <rahuldhal800@gmail.com>
2026-09-11 18:11:59 +0530

    feat: export AEGIS 1T configuration

--- aegis_model/__init__.py
+++ aegis_model/__init__.py
@@ -1,6 +1,13 @@
-"""AEGIS small language model: tokenizer, transformer, training, and generation."""
+"""AEGIS language-model family: tokenizer, Transformer, configs, training, generation."""
 
 from .model import AEGISConfig, AEGISForCausalLM
 from .tokenizer import ByteTokenizer
+from .configs import AEGIS_1T_CONFIG, estimate_parameter_count
 
-__all__ = ["AEGISConfig", "AEGISForCausalLM", "ByteTokenizer"]
+__all__ = [
+    "AEGISConfig",
+    "AEGISForCausalLM",
+    "ByteTokenizer",
+    "AEGIS_1T_CONFIG",
+    "estimate_parameter_count",
+]
