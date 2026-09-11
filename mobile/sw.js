2697146880d90dd00b9073f2d1577df6a652e8ba
rahuldhal800-a11y <rahuldhal800@gmail.com>
2026-09-11 16:57:46 +0530

    feat: add AEGIS mobile offline shell

---
+++ mobile/sw.js
@@ -0,0 +1,1 @@
+const CACHE='aegis-mobile-v1';const ASSETS=['./','./index.html','./app.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
